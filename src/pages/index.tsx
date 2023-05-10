80e6e34f8a2ffb0a866cd87f2528996751a055c6
Helio-junior-ADS <64769193+Helio-junior-ADS@users.noreply.github.com>
2023-05-10 15:31:18 -0300

    Esquema de cores

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import Layout from "../components/template/Layout"
+
+export default function Home() {
+  return (
+    <div>
+      <Layout titulo="Página inicial" subtitulo="Estamos construindo um tamplate admin">
+          <h3>Conteúdo !!!!</h3>
+      </Layout>
+    </div>
+  )
+}
