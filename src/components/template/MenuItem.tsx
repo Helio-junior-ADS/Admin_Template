b4bebb802175ffffec290273d33bcdb82025c2e0
Helio-junior-ADS <64769193+Helio-junior-ADS@users.noreply.github.com>
2023-05-11 17:44:14 -0300

    Menu lateral

--- src/components/template/MenuItem.tsx
+++ src/components/template/MenuItem.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 interface ManuItemProps {
   url : string,
   texto: string,
@@ -6,8 +8,20 @@ interface ManuItemProps {
 
 export default function ManuItem (props: ManuItemProps) {
   return (
-    <li className={``}>
-      {props.icone}
+    <li className={`hover:bg-gray-100`}>
+      <Link href={props.url}>
+        <div className={`
+          flex flex-col justify-center items-center
+          h-20 w-20
+        `}>
+          {props.icone}
+          <span className={`
+            text-xs font-light text-gray-600
+          `}>
+            {props.texto}
+          </span>
+        </div>
+      </Link>
     </li>
   )
 }
