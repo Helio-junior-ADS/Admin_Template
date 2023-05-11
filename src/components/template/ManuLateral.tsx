89ee30035446ae415580aba507434685d4c61e84
Helio-junior-ADS <64769193+Helio-junior-ADS@users.noreply.github.com>
2023-05-11 18:04:32 -0300

    Component Logo

--- src/components/template/ManuLateral.tsx
+++ src/components/template/ManuLateral.tsx
@@ -1,9 +1,17 @@
-import { IconeAjustes, IconeBell, IconeCasa, IconeSino } from "../icons"
+import { IconeAjustes, IconeCasa, IconeSino } from "../icons"
 import ManuItem from "./MenuItem"
+import Logo from "./Logo"
 
 export default function ManuLateral () {
   return (
     <aside>
+      <div className={`
+         flex flex-col items-center justify-center  
+         bg-gradient-to-r from-indigo-500 to-purple-800
+         h-20 w-20
+      `}>
+        <Logo/>
+      </div>
       <ul>
         <ManuItem url="/" texto="Inicio" icone={IconeCasa}/>
         <ManuItem url="/ajustes" texto="Ajustes" icone={IconeAjustes}/>        
